b4efa307d49fbcecad956d6bb48a9220c01d5cf6
Icosahunter <anzelknight@gmail.com>
2026-04-29 19:02:01 -0500

    Update with new pin count (removing pins that cannot be accessed with existing arduino ide variant), start work on arduino board config

---
+++ arduino-ide-board/variant.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define PINS_COUNT 11u
+#define NUM_DIGITAL_PINS 11u
+#define NUM_ANALOG_INPUTS 8u
+
+// TODO, below
+#define analogInputToDigitalPin(p)
+
+#define PIN_SERIAL_RX
+#define PIN_SERIAL_TX
+
+#define PIN_SPI_SS
+#define PIN_SPI_MOSI
+#define PIN_SPI_MISO
+#define PIN_SPI_SCK
+
+#define PIN_WIRE_SDA
+#define PIN_WIRE_SCL
